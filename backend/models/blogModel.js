17750594298b71c6148613b63ae15476680cf5d2
Abhishek-Yadav-web <88048063+Abhishek-Yadav-web@users.noreply.github.com>
2022-02-26 21:38:28 +0530

    Update blogModel.js

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -20,7 +20,6 @@ const blogSchema = mongoose.Schema(
         tags : [
             {
                 type: String,
-                required : true,
                 default : "news"
             }
         ],
