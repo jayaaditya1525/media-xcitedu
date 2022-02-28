49d7616bcb9742be23daeb64600fdb4771a15931
Abhishek-Yadav-web <x0abhie@gmail.com>
2022-02-28 18:20:16 +0530

    desinged preloader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import {Routes,Route} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import NotFound from './Pages/Error/NotFound';
 import MainPage from './Pages/MainPage/MainPage';
-import data from './JSON/data.json'
 import NewsBlogPage from './Pages/NewsBlogPage/NewsBlogPage';
 import BusinessBlogPage from './Pages/BusinessBlogPage/BusinessBlogPage';
 import SociologyBlogPage from './Pages/SociologyBlogPage/SociologyBlogPage';
@@ -17,7 +16,7 @@ function App() {
    <>
     <div className="container">
       <Routes>
-        <Route exact path="/" element={<MainPage data={data.mainPage}/>} />
+        <Route exact path="/" element={<MainPage/>} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/news" element={<NewsBlogPage />} />
         <Route exact path="/business" element={<BusinessBlogPage />} />
--- frontend/src/Components/PerLoader/PerLoader.js
+++ frontend/src/Components/PerLoader/PerLoader.js
@@ -3,10 +3,35 @@ import './PerLoader.css'
 const PerLoader = ({hide}) => {
   return (
     <>
+    
         <div className="PerLoaderForMainPage center-row" id={hide === true ? "hide" : null}>
+          <div className="navbar">
+            <nav className='center-row-left-right'>
+              <div className='center-row'>
+                <div className="box"></div>
+                <div className="textBx"></div>
+              </div>
+              <div className='center-row'>
+                <div className="minTextBx"></div>
+                <div className="minTextBx"></div>
+                <div className="minTextBx"></div>
+                <div className="minTextBx"></div>
+                <div className="minTextBx"></div>
+              </div>
+              <div className='center-row'>
+                <div className="minCricle"></div>
+                <div className="minCricle"></div>
+              </div>
+            </nav>
+          </div>
             <div className="imgBx center-row">
-                <img src="https://i0.wp.com/www.rankred.com/wp-content/uploads/2017/06/redirecting-loader.gif?resize=650%2C286" alt="" />
+                <img src="https://res.cloudinary.com/webdevoliva/image/upload/a_45/v1646047123/loader/superhero_txbvkn.png" alt="" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
+            <span className="text">Redirecting</span>
         </div>
     </>
   )
